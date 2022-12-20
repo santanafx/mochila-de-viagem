0863834bc279beede79ca2bd5482182897440401
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-20 10:23:33 -0300

    Adicionando item na lista

--- js/main.js
+++ js/main.js
@@ -2,6 +2,33 @@ const form = document.getElementById("novoItem");
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
-  console.log(evento.target.elements["nome"].value);
-  console.log(evento.target.elements["quantidade"].value);
+
+  criaElemento(
+    evento.target.elements["nome"].value,
+    evento.target.elements["quantidade"].value
+  );
 });
+
+const criaElemento = (nome, quantidade) => {
+  console.log(nome);
+  console.log(quantidade);
+
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = quantidade;
+
+  novoItem.appendChild(numeroItem);
+  //<li class="item">nome
+  //        <strong>quantidade</strong>
+  //</li>
+  //
+  novoItem.innerHTML = nome;
+
+  const lista = document.getElementById("lista");
+
+  lista.appendChild(novoItem);
+
+  console.log(novoItem);
+};
